a471b9b3a3803af18cca70bbf66b62896696e548
mJaJksJ <maksim.m00@mail.ru>
2022-10-23 09:28:05 +0300

    chore: изображение для первой страницы v2

--- src/Components/StartPage/StartPage.jsx
+++ src/Components/StartPage/StartPage.jsx
@@ -4,7 +4,10 @@ import {Link} from "react-router-dom";
 const StartPage = (props) => {
 
     return (
-        <div style={{display:"flex", justifyContent:"center"}}>
+        <div style={{display:"flex", justifyContent:"center", flexDirection:'column'}}>
+            <div>
+                <img src={'https://github.com/mJaJksJ/Myosotis/blob/StartPageAndSurveys/public/images/start_picture.jpg'} alt="Я Аянами Рей? А ты кто? Айянами Рей. Значит ты тоже Аянами Рей. Да, я та кого зовут Аянами Рей."/>
+            </div>
             <Link to={'/survey'}>Начать</Link>
         </div>
     );
