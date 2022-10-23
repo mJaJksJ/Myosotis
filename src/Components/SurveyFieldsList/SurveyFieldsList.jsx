0c3d19d1c4dfbeab5fff861191aa975f74434f49
mJaJksJ <maksim.m00@mail.ru>
2022-10-23 03:03:21 +0300

    chore: маршрутизация работает

--- src/Components/SurveyFieldsList/SurveyFieldsList.jsx
+++ src/Components/SurveyFieldsList/SurveyFieldsList.jsx
@@ -2,11 +2,63 @@ import React from 'react';
 import {Button} from "primereact";
 
 const SurveyFieldsList = (props) => {
-    const list = props.list;
+    const data = [{
+        "field_id": 1,
+        "field_name": "Выполненные работы",
+        "field_type": "checkbox",
+        "field_description": "Заполните выполненные работы",
+        "field_values": {
+            "0": "Выравнивание ДК",
+            "1": "Показания наклона",
+            "2": "Включение ИБП",
+            "3": "Восстановление питания",
+            "100": "Установка транспортной колонки"
+        }
+    },
+        {
+            "field_id": 2,
+            "field_name": "Использование дополнительного авто",
+            "field_type": "radiobox",
+            "field_description": "Использование дополнительного авто",
+            "field_values": [
+                "Да",
+                "Нет",
+                "Да, другой организации"
+            ]
+        },
+        {
+            "field_id": 3,
+            "field_name": "Фото после выполнения работ",
+            "field_type": "file",
+            "field_description": "Прикрепите фото после выполнения работ",
+            "field_values": [
+                "d7bbc0ce9327410799450755a8ec1f5d.jpg",
+                "757700e0ed874a18abcf5e39c4673a36.jpg",
+                "98e0a3678c7f4e5bacd55c7f546d40be.jpg"
+            ]
+        },
+        {
+            "field_id": 4,
+            "field_name": "Описание выполненых работ",
+            "field_type": "text",
+            "field_description": "Описание что было до приезда, что было сделано, что нужно доделать"
+        },
+        {
+            "field_id": 5,
+            "field_name": "Использованные материалы",
+            "field_type": "checkbox",
+            "field_description": "Использованные материалы",
+            "field_values": [
+                "Кабель",
+                "Бокс",
+                "Болт 20x5"
+            ]
+        }];
+
     return (
         <div style={{display:"flex", justifyContent:"center"}}>
             <ul>
-                {list.map(x =>
+                {data.map(x =>
                 <li>
                     {x.field_name}
                 </li>)}
